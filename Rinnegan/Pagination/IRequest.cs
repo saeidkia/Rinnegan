66cc79b26bf48ce454ae08e3794a515eb9849b0f
saeidkia <kia.saeid@gmail.com>
2024-03-16 09:27:07 +0330

    pagination added

---
+++ Rinnegan/Pagination/IRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rinnegan.Pagination
+{
+    public interface IRequest
+    {
+
+        [Range(1, int.MaxValue, ErrorMessage = "{0} را بین {1} و {2} وارد کنید")]
+        [Display(Name = "شماره صفحه")]
+        [Required(ErrorMessage = "{0} را وارد کنید")]
+        int pageNumber { get; set; }
+
+
+        [Display(Name = "تعداد نمایش در صفحه")]
+        [Required(ErrorMessage = "{0} را وارد کنید")]
+        int pageSize { get; set; }
+    }
+}
---
+++ Rinnegan/Pagination/Response.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rinnegan.Pagination
+{
+    public class Response<T>
+    {
+        int pageNumber { get; set; }
+        int pageSize { get; set; }
+        int total { get; set; }
+        List<T> items { get; set; }
+    }
+}
